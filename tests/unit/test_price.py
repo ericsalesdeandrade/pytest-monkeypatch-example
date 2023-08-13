4ff8c425c3e2ae014a1cc8313981a849deaf6e6b
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-08-13 21:47:18 +0530

    Create test_price.py

---
+++ tests/unit/test_price.py
@@ -0,0 +1,24 @@
+def tax_record_from_DW():
+    return True
+
+
+def price_record_from_DW():
+    return True
+
+
+def calc_price():
+    Taxes = tax_record_from_DW()
+    Price = price_record_from_DW()
+
+    return Taxes + Price
+
+
+def test_calc_price(monkeypatch):
+    monkeypatch.setattr(
+        "tests.unit.test_price.tax_record_from_DW", lambda: 10
+    )  # Monkeypatching the function
+    monkeypatch.setattr(
+        "tests.unit.test_price.price_record_from_DW", lambda: 20
+    )  # Monkeypatching the function
+    value = calc_price()
+    assert value == 30
